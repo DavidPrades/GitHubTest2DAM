6e7086206de569f4a4937929af17f6d7e3d671c0
Ferran <vidalferran46@gmail.com>
2019-01-29 17:16:56 +0100

    Commit by Ferran

--- app/src/main/java/org/ieselcaminas/pmdm/githubtest2dam/MainActivity.java
+++ app/src/main/java/org/ieselcaminas/pmdm/githubtest2dam/MainActivity.java
@@ -2,6 +2,7 @@ package org.ieselcaminas.pmdm.githubtest2dam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
@@ -78,6 +79,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void b15_method_Ferran() {
-
+        Toast.makeText(this, "Ferran",Toast.LENGTH_SHORT).show();
     }
 }
